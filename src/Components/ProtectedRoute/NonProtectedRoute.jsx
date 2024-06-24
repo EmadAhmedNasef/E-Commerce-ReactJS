56fd4c2c702c7cb6ad54a95e2d0f71b6ae5fbe0c
EmadAhmedNasef <emadnasef86@gmail.com>
2024-06-24 12:44:25 +0300

    fixed home

--- src/Components/ProtectedRoute/NonProtectedRoute.jsx
+++ src/Components/ProtectedRoute/NonProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom'
 
 export default function NonProtectedRoute(props) {
     if(localStorage.getItem("token")){
-        return <Navigate to={"/home"} />
+        return <Navigate to={"/"} />
     }
     else {
         return props.children
